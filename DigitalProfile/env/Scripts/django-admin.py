5c1a76097be94e03cc058c2b2ba9c08706ddca34
Сергей Бильдин <serega.bildin@outlook.com>
2020-04-17 20:53:22 +0500

    Добавьте файлы проекта.

---
+++ DigitalProfile/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sereg\OneDrive\Рабочий стол\2 курс\Курсовая Digital Profile\DigitalProfile\DigitalProfile\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
