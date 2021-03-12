65e26e12eca96911ff0b0144d83f3c36099f3196
SergeyBildin <serega.bildin@outlook.com>
2021-03-12 14:40:36 +0600

    restruct and restore project

--- DigitalProfile/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Sereg\OneDrive\Рабочий стол\2 курс\Курсовая Digital Profile\DigitalProfile\DigitalProfile\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
